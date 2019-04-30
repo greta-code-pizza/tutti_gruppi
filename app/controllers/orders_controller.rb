5f208b678c65ecaed730054bfe65a0376ec4885b
ArnaudFgr <arnaudfgr@gmail.com>
2019-04-30 13:14:41 +0200

    form in progress

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,15 +7,15 @@ class OrdersController < ApplicationController
 
   def create
     #binding.pry
-
-    # @order = Order.create consumer_id:params[:consumer_id]
-    # 3.times do
-    # @order_item = OrderItem.create product_id:params[:product_id], quantity:params[:quantity], order_id: @order.id
-    # end
-    @order = Order.create order_params
-    @order_item = OrderItem.create order_item_params.merge(
-      order_id: @order.id
-    )
+    @product = Product.all
+    @order = Order.create consumer_id:params[:consumer_id]
+    @product.each do |p|
+    @order_item = OrderItem.create product_id:p.id, quantity:params[:quantity], order_id: @order.id
+    end
+    # @order = Order.create order_params
+    # @order_item = OrderItem.create order_item_params.merge(
+    #   order_id: 1
+    # )
     if @order && @order_item
       flash[:info] = "success"
       redirect_to request.referrer
@@ -27,12 +27,12 @@ class OrdersController < ApplicationController
 
   private
 
-  def order_params
-    params.require(:order).permit(:consumer_id)
-  end
+  # def order_params
+  #   params.require(:order).permit(:consumer_id)
+  # end
 
-  def order_item_params
-    params.require(:order_item).permit(:product_id, :quantity, :order_id)
-  end
+  # def order_item_params
+  #   params.require(:order_item).permit(:product_id, :quantity, :order_id)
+  # end
   
 end
