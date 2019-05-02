cfc682e39b33a6ce764fa91d0bfe5cc2a4ec6e62
ArnaudFgr <arnaudfgr@gmail.com>
2019-05-02 15:21:08 +0200

    WorkInProgress

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,38 +1,25 @@
 class OrdersController < ApplicationController
   def new
     @order = Order.new
-    @product = Product.all
+    @products = Product.all
     @consumer = Consumer.all
   end
 
   def create
     #binding.pry
-    @product = Product.all
-    @order = Order.create consumer_id:params[:consumer_id]
-    @product.each do |p|
-    @order_item = OrderItem.create product_id:p.id, quantity:params[:quantity], order_id: @order.id
-    end
-    # @order = Order.create order_params
-    # @order_item = OrderItem.create order_item_params.merge(
-    #   order_id: 1
-    # )
-    if @order && @order_item
-      flash[:info] = "success"
+    @order = Order.create.create_with_deps(params)
+    if @order
+      flash[:info] = 'success'
       redirect_to request.referrer
     else
-      flash[:info] = "failed"
+      flash[:info] = 'failed'
       render 'new'
     end
   end
-
+ 
   private
 
-  # def order_params
-  #   params.require(:order).permit(:consumer_id)
-  # end
+  def params
+  end
 
-  # def order_item_params
-  #   params.require(:order_item).permit(:product_id, :quantity, :order_id)
-  # end
-  
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,12 @@
 class Order < ActiveRecord::Base
-    has_many :products
-    belongs_to :consumer
+  has_many :products
+  belongs_to :consumer
+
+  def create_with_deps(params)
+    order = Order.create(consumer_id: params[:consumer_id])
+
+    params[:product_id].each do |key, value|
+      OrderItem.create(order_id: order.id, product_id: key, quantity: value)
+    end
+  end
 end
