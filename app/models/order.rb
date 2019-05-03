0425b37f9d57df41448dce1d3a6cd73b9dc7666f
Nicolas LE CHENIC <nicolaslechenic@gmail.com>
2019-05-03 13:20:55 +0200

    fix and reimport after rebase

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,11 +1,6 @@
 class Order < ActiveRecord::Base
   has_many :products
   belongs_to :consumer
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> accept modifications
 
   def self.create_with_deps(params)
     order = Order.create(consumer_id: params[:order][:consumer_id])
@@ -14,8 +9,4 @@ class Order < ActiveRecord::Base
       OrderItem.create(order_id: order.id, product_id: key, quantity: value)
     end
   end
-<<<<<<< HEAD
->>>>>>> WorkInProgress
-=======
->>>>>>> accept modifications
 end
