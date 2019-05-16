c0851b916f964d546c200295202116f9db4a843e
Nicolas LE CHENIC <nicolaslechenic@gmail.com>
2019-05-16 17:40:34 +0200

    clean before merge PR

--- db/migrate/20190516130951_create_table_whitelist.rb
+++
@@ -1,7 +0,0 @@
-class CreateTableWhitelist < ActiveRecord::Migration[5.2]
-  def change
-    create_table :whitelists do |t|
-      t.string :email
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,22 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_16_130951) do
+ActiveRecord::Schema.define(version: 2019_05_13_123311) do
 
   create_table "consumers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "firstname", default: "", null: false
-    t.string "lastname", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
-    t.index ["email"], name: "index_consumers_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_consumers_on_reset_password_token", unique: true
+    t.string "firstname"
+    t.string "lastname"
+    t.string "email"
   end
 
   create_table "order_items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -47,10 +37,6 @@ ActiveRecord::Schema.define(version: 2019_05_16_130951) do
     t.integer "price"
     t.integer "weight_value"
     t.string "weight_type"
-  end
-
-  create_table "whitelists", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "email"
   end
 
   add_foreign_key "order_items", "orders"
