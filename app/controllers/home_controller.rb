1720178b9d2ee95c425d96332390355716a9944f
Nicolas LE CHENIC <nicolaslechenic@gmail.com>
2019-05-23 11:31:49 +0200

    boo

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,4 +6,27 @@ class HomeController < ApplicationController
     @foo = true
     @foo = true
   end
+
+  def too_long
+    a = 1
+    b = 2
+    c = 3
+    d = 4
+    e = 5
+    f = 6 
+    g = 7
+    h = 8
+    i = 9
+    j = 10
+    k = 11
+    l = 12
+    m = 13
+    n = 14
+    o = 15
+    p = 16 
+    q = 17
+    r = 18
+    s = 19
+    t = 20
+  end
 end
