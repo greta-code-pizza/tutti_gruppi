b5ef15d4817e737062489dd55d58f4d3f68dc2c5
Nicolas LE CHENIC <nicolaslechenic@gmail.com>
2019-05-23 12:09:55 +0200

    clean code before merge

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,12 +1,12 @@
 class OrdersController < ApplicationController
   before_action :authenticate_consumer!
+  before_action :find_order, only: [:show]
   
   def index
     @orders = Order.all
   end
 
   def show
-    @order = Order.find(params[:id])
     @orderitems = OrderItem.where(order_id: params[:id])
   end
 
@@ -17,16 +17,22 @@ class OrdersController < ApplicationController
   end
 
   def create
-    @order = Order.create(consumer_id: current_consumer.id)
+    @order = Order.create(consumer: current_consumer)
     @order_item = Order.create_with_deps(params, @order)
     if @order
-      ConsumerMailer.notify_consumer(current_consumer.id, @order).deliver
-      ManagerMailer.notify_manager(current_consumer.id, @order).deliver
+      ConsumerMailer.notify_consumer(current_consumer, @order).deliver
+      ManagerMailer.notify_manager(current_consumer, @order).deliver
       flash[:info] = 'success'
       redirect_to request.referrer
     else
       flash[:info] = 'failed'
       render 'new'
     end
+  end
+
+  private
+
+  def find_order
+    @order = Order.find(params[:id])
   end
 end
