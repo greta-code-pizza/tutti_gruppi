3ee321df099d226d84c59c0dd788c83be6f6f413
ArnaudFgr <arnaudfgr@gmail.com>
2019-05-27 10:35:37 +0200

    resolve pr

--- app/controllers/authentications/registrations_controller.rb
+++ app/controllers/authentications/registrations_controller.rb
@@ -8,25 +8,31 @@ class Authentications::RegistrationsController < Devise::RegistrationsController
       build_resource(sign_up_params)
       resource.userable_id = member.id
       resource.save
-      yield resource if block_given?
-      if resource.persisted?
-        if resource.active_for_authentication?
-          set_flash_message! :notice, :signed_up
-          sign_up(resource_name, resource)
-          respond_with resource, location: after_sign_up_path_for(resource)
-        else
-          set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
-          expire_data_after_sign_in!
-          respond_with resource, location: after_inactive_sign_up_path_for(resource)
-        end
-      else
-        clean_up_passwords resource
-        set_minimum_password_length
-        respond_with resource
-      end
+      # yield resource if block_given?
+      valid_authentication(resource)
     else
       flash[:notice] = 'An error occured'
       redirect_to root_path
+    end
+  end
+
+  private
+
+  def valid_authentication(resource)
+    if resource.persisted?
+      if resource.active_for_authentication?
+        set_flash_message! :notice, :signed_up
+        sign_up(resource_name, resource)
+        respond_with resource, location: after_sign_up_path_for(resource)
+      else
+        set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
+        expire_data_after_sign_in!
+        respond_with resource, location: after_inactive_sign_up_path_for(resource)
+      end
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+      respond_with resource
     end
   end
 end
