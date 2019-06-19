f8c891f0f9de69c868612f782b4db5a12b11e0dc
slashflex <pro.davidsaoud@gmail.com>
2019-06-19 18:30:39 +0200

    new routes added for products_controller methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :authentications, controllers: { registrations: 'authentications/registrations', confirmations: 'authentications/confirmations'}
-
+  devise_for :authentications, controllers: { registrations: 'authentications/registrations', confirmations: 'authentications/confirmations' }
   root 'home#index'
-
   get 'home/index'
   get 'home/private'
   get 'orders/new', to: 'orders#new'
@@ -15,15 +13,16 @@ Rails.application.routes.draw do
     get 'show/:id', to: 'dashboard#show'
     get 'edit/:id', to: 'dashboard#edit'
     get 'whitelist', to: 'whitelist#index'
-    get 'index-produits', to: 'dashboard#index_products'
-    get 'edition-produits/:id', to: 'dashboard#edit_products'
-    patch 'edition-produits/:id', to: 'dashboard#update_product'
-    # get 'formulaire-nouveau-produit', to: 'dashboard#form_new_product'
-    get 'formulaire-nouveau-produit', to: 'dashboard#form_new_product'
-    post 'ajout-produit', to: 'dashboard#add_new_product'
+    get 'index-produits', to: 'products#index_products'
+    get 'edition-produits/:id', to: 'products#edit_products'
+    patch 'edition-produits/:id', to: 'products#update_product'
+    get 'form_new_product', to: 'products#form_new_product'
+    post 'form_new_product', to: 'products#add_new_product'
+    delete 'delete_product/:id', to: 'products#destroy'
+    
     resources :whitelist, only: %i[index home create new]
   end
 
-  resources :orders, only:[:index, :show, :create, :new]
-  resources :authentications, only:[:index, :show, :create, :new, :edit, :update, :destroy]
+  resources :orders, only: %i[index show create new]
+  resources :authentications, only: %i[index show create new edit update destroy]
 end
