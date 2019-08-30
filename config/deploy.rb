4a2e57d5c477e6f684b4d1eae4d1f1499fd38cfb
Nicolas LE CHENIC <nicolaslechenic@gmail.com>
2019-08-30 12:30:29 +0200

    assets on staging and emailing on shared

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,6 +5,6 @@ set :application, 'tutti'
 set :repo_url, 'git@github.com:Kercode/tutti_gruppi.git'
 
 # Default value for :linked_files is []
-append :linked_files, 'config/database.yml', 'config/secrets.yml'
+append :linked_files, 'config/database.yml', 'config/secrets.yml', 'config/emailing.yml'
 # Default value for linked_dirs is []
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads'
